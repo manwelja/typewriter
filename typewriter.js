eceaf7903c22745ca71ad072df8ef0424706118c
Jennifer Manwell <manwell.fortier@gmail.com>
2022-04-12 09:25:00 -0400

    Animated Sentences - Implemented exercise requirements

---
+++ typewriter.js
@@ -0,0 +1,12 @@
+const sentence = "hello there from lighthouse labs";
+let count = 0;
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, count);
+  count += 50;
+}
+setTimeout(() => {
+  process.stdout.write('\n');
+}, sentence.length * 50);
+
